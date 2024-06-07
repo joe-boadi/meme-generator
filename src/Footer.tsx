578a1b9a76bb7a0f4f586b13297b25df08db22f1
joe-boadi <joeboadi1@gmail.com>
2024-06-07 22:22:01 +0000

    Initialize components

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return(
+        <footer>
+            <small>
+                <p>Made by <a href="https://github.com/joe-boadi">joe-boadi</a></p>
+            </small>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+
+const Form = () => {
+    return (
+        <div className="">
+            <input type="text" placeholder="Top text"/>
+            <input type="text" placeholder="Bottom text"/>
+            <button type="button"> Generate meme</button>
+        </div>
+    )
+}
+
+export default Form;
