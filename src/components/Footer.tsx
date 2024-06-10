8b80331b325064ec4ee503410f412a47d75ccdd6
joe-boadi <joeboadi1@gmail.com>
2024-06-10 09:29:50 +0000

    Update component path + style component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+    return(
+        <footer className="footer p-10 m-7 items-center justify-center text-center">
+            <small>
+                <p>
+                    Made by 
+                    <a href="https://github.com/joe-boadi" className="hover:underline"> joe-boadi </a> 
+                    with 
+                    <a href="https://reactjs.org" className="hover:underline"> react</a>
+                
+                </p>
+            </small>
+        </footer>
+    )
+}
+
+export default Footer;
